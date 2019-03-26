545e25aed9ced99f30cb3fb116a4d37d8efe301c
Markus <markusbjon@gmail.com>
2019-03-26 12:58:01 +0100

    faster detector

--- harris_corner_detector.py
+++ harris_corner_detector.py
@@ -1,14 +1,16 @@
 import numpy as np 
-from scipy import ndimage
 import cv2
+import time
 
 
 def get_grads(img):
 	grad_x = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=3)
 	grad_y = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=3)
+	# grad_x = cv2.Scharr(img,cv2.CV_64F,1,0)
+	# grad_y = cv2.Scharr(img,cv2.CV_64F,0,1)	
 	return grad_x, grad_y
 
-def corner_detector(image, k=0.06, patch_size=3, threshold=0):
+def corner_detector(image, k=0.06, patch_size=3, threshold_div_factor=1e4):
 	"""
 	input parameter: grayscale image
 	output: image with corner features and corners [x,y, R]
@@ -29,22 +31,21 @@ def corner_detector(image, k=0.06, patch_size=3, threshold=0):
 
 	summing_kernel = np.ones((patch_size, patch_size))
 
-	A_mat = ndimage.convolve(I_xx, summing_kernel, mode='constant')
-	B_mat = ndimage.convolve(I_xy, summing_kernel, mode='constant')
-	C_mat = ndimage.convolve(I_yy, summing_kernel, mode='constant')
+	A_mat = cv2.filter2D(I_xx, -1, summing_kernel)
+	B_mat = cv2.filter2D(I_xy, -1, summing_kernel)
+	C_mat = cv2.filter2D(I_yy, -1, summing_kernel)
 
 	determinants = (A_mat * C_mat) - B_mat**2
 	traces = A_mat + C_mat
 	R_matrix = determinants - k*(traces)**2
-	
-	threshold = np.max(R_matrix)/1e3
-	print('threshold: ', threshold)
-	for x in range(R_matrix.shape[0]):
-		for y in range(R_matrix.shape[1]):
-			if R_matrix[x, y] > threshold:
-				corners.append((R_matrix[x, y], x, y))
-				new_img.itemset((x, y, 0), 0)
-				new_img.itemset((x, y, 1), 0)
-				new_img.itemset((x, y, 2), 255)
-	return new_img, corners
 
+	threshold = np.max(R_matrix)/threshold_div_factor
+
+	corner_indices = np.argwhere(R_matrix>threshold)
+
+	for x, y in corner_indices:
+		corners.append((R_matrix[x, y], x, y))
+		new_img.itemset((x, y, 0), 0)
+		new_img.itemset((x, y, 1), 0)
+		new_img.itemset((x, y, 2), 255)
+	return new_img, corners		
